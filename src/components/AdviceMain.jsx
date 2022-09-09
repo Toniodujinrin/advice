f69fc5fd0cd19bc34af8b054996c8185b3022075
Toniodujinrin <todujinrin@gmail.com>
2022-09-09 02:27:46 +0100

    update

--- src/components/AdviceMain.jsx
+++ src/components/AdviceMain.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
-import AdviceCard from './AdviceCard';
-import Score from './utils/Score';
-const AdviceMain = ({handleNew,advice,handleDecreament,handleIncreament}) => {
-    return ( 
-        <div id='main'>
-        <div id='score-card'>
-        <Score score={advice.score} handleDecreament={handleDecreament} handleIncreament={handleIncreament} id={advice.id}/>
-        <AdviceCard advice = {advice}/>
-        
-        </div>
-        <button className='new-button' onClick={handleNew}>New</button>
-        </div>
-     );
-}
- 
+import React from "react";
+import AdviceCard from "./AdviceCard";
+import Score from "./utils/Score";
+const AdviceMain = ({
+  handleNew,
+  advice,
+  handleDecreament,
+  handleIncreament,
+}) => {
+  return (
+    <div id="main">
+      <div id="score-card">
+        <Score
+          score={advice.score}
+          handleDecreament={handleDecreament}
+          handleIncreament={handleIncreament}
+          id={advice.id}
+        />
+        <AdviceCard advice={advice} />
+      </div>
+      <button className="new-button" onClick={handleNew}>
+        New
+      </button>
+    </div>
+  );
+};
+
 export default AdviceMain;
